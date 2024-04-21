980f04da893e8908baf0ac41b6fb0b1a3423431d
Zechariah Fisher-Coleman <zechfc@gmail.com>
2024-04-20 21:29:22 -0400

    cleaned up login and cart

--- my-react-app/src/pages/cart.jsx
+++ my-react-app/src/pages/cart.jsx
@@ -30,7 +30,6 @@ const Cart = () => {
             </div>
             </section>
 
-          <Footer />
 
 
     </div>
--- my-react-app/src/pages/signin.jsx
+++ my-react-app/src/pages/signin.jsx
@@ -26,7 +26,6 @@ const Signin = () => {
                 
             </section>
 
-          <Footer />
 
 
     </div>
